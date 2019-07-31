02048b38b73bb32c5f235be6ba7c5423138393cd
James Truher <jimtru@microsoft.com>
2019-07-30 20:32:47 -0700

    initialized module

---
+++ test/hackathon19.Tests.ps1
@@ -0,0 +1,14 @@
+Describe "Test hackathon19" -tags CI {
+    BeforeAll {
+    }
+    BeforeEach {
+    }
+    AfterEach {
+    }
+    AfterAll {
+    }
+    It "This is the first test for hackathon19" {
+        $name = "Hello World"
+        verb-noun -name $name | Should -BeExactly $name
+    }
+}
